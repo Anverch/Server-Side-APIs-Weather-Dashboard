b7ff219d101531e2a0f70b7e795e80d8c38cd3c0
Aleksandr Saiyan <anverch@gmail.com>
2020-04-14 16:17:23 -0500

    add current conditions

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,37 @@
 var apiKey = "76e9ff367a27de5d400ffd75af441c88";
 
 
-$("#search").on("click", function(event) {
+$("#search").on("click", function (event) {
 
-    event.preventDefault();
-    var searchedCity = $("#city-input").val();
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchedCity + "&units=imperial&appid=" + apiKey;
+  event.preventDefault();
+  var searchedCity = $("#city-input").val();
+  var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchedCity + "&units=imperial&appid=" + apiKey;
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function (response) {
+    console.log(response);
+
+    // Transfer content to HTML
+    $("#city").text("City: " + response.name + moment().format(" (M/D/YYYY)"));
+    $("#temperature").text("Temperature: " + response.main.temp + " " + String.fromCharCode(176) + "F");
+    $("#humidity").text("Humidity: " + response.main.humidity);
+    $("#wind-speed").text("Wind Speed: " + response.wind.speed);
+
     
-        $.ajax({
-        url: queryURL,
-        method: "GET"
-      }).then(function(response) {
-          console.log(response);
+    var lat = response.coord.lat;
+    var lon = response.coord.lon;    
+    var queryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey;
 
-        // Transfer content to HTML
-        $("#city").text("City: " + response.name + moment().format(" (M/D/YYYY)"));
-        $("#temperature").text("Temperature: " + response.main.temp + " " + String.fromCharCode(176)+"F");
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function (response) {
+      $("#u-v-index").text("UV Index: " + response.current.uvi);
 
-      });
+    })
+
+  });
 
 });
-
