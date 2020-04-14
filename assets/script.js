3dbe3f4372712ad911b050dd086a5aa64d1eb611
Aleksandr Saiyan <anverch@gmail.com>
2020-04-13 20:10:00 -0500

    add queryUrl

---
+++ assets/script.js
@@ -0,0 +1,5 @@
+var apiKey = "76e9ff367a27de5d400ffd75af441c88";
+var searchedCity = "Chicago"
+var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + searchedCity + "&appid=" + apiKey;
+
+
