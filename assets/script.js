7c556a336c184e29032b45dacee0eb75da9d3c6a
Aleksandr Saiyan <anverch@gmail.com>
2020-04-14 09:12:28 -0500

    add search city input

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,20 @@
 var apiKey = "76e9ff367a27de5d400ffd75af441c88";
-var searchedCity = "Chicago"
-var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + searchedCity + "&appid=" + apiKey;
 
+
+$("#search").on("click", function(event) {
+
+    event.preventDefault();
+    var searchedCity = $("#city-input").val();
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchedCity + "&appid=" + apiKey;
+    
+        $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response) {
+          console.log(response);
+        $("#city").text("City : " + response.name + moment().format(" (M/D/YYYY)"));
+
+      });
+
+});
 
