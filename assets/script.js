ecd1222c0dad55a65c98b838686bb034c2f26fab
Aleksandr Saiyan <anverch@gmail.com>
2020-04-14 10:27:47 -0500

    add temperature

--- assets/script.js
+++ assets/script.js
@@ -5,14 +5,17 @@ $("#search").on("click", function(event) {
 
     event.preventDefault();
     var searchedCity = $("#city-input").val();
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchedCity + "&appid=" + apiKey;
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchedCity + "&units=imperial&appid=" + apiKey;
     
         $.ajax({
         url: queryURL,
         method: "GET"
       }).then(function(response) {
           console.log(response);
-        $("#city").text("City : " + response.name + moment().format(" (M/D/YYYY)"));
+
+        // Transfer content to HTML
+        $("#city").text("City: " + response.name + moment().format(" (M/D/YYYY)"));
+        $("#temperature").text("Temperature: " + response.main.temp + " " + String.fromCharCode(176)+"F");
 
       });
 
