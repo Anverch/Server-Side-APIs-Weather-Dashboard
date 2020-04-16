4f40f4e1102657174db0bec4076d57823af6bf5d
Aleksandr Saiyan <anverch@gmail.com>
2020-04-16 15:08:40 -0500

    add currnet day icon

--- assets/script.js
+++ assets/script.js
@@ -7,36 +7,40 @@ $("#search").on("click", function (event) {
   var searchedCity = $("#city-input").val();
   var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchedCity + "&units=imperial&appid=" + apiKey;
 
+  
   $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function (response) {
 
+    var icon = response.weather[0].icon;
+    var imgURL = "https:openweathermap.org/img/wn/" + icon + "@2x.png"
+    var iconImg = $("img").attr("src", imgURL);
+
     // Transfer content to HTML
     $("#city").text(" " + response.name + moment().format(" (M/D/YYYY)"));
+    $("#city").addClass("font-weight-bold")
     $("#temperature").text(" " + response.main.temp + " " + String.fromCharCode(176) + "F");
     $("#humidity").text(" " + response.main.humidity + " %");
     $("#wind-speed").text(" " + response.wind.speed+ " MPH");
 
-    
+    //Current Weather icon to HTML
+    $("#icon-image").html(iconImg);
+    $("#icon-image").addClass("icon-size");
+
+        
     var lat = response.coord.lat;
     var lon = response.coord.lon;    
-    var queryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey;
+    var uvQueryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey;
 
     $.ajax({
-      url: queryURL,
+      url: uvQueryURL,
       method: "GET"
     }).then(function (response) {
       $("#u-v-index").text(response.current.uvi);
 
-      var imgURL = (response.weather.icon);
-      // var image = $("<img>").attr("src", imgURL);
-      // $("#city").append(image);
+    });
 
-
-
-      current.weather.icon
-    })
 
   });
 
